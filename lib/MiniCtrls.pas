9954bf6037220f7c3e155c122929907c4cf183de
Zaher Dirkey <zaherdirkey@yahoo.com>
2019-02-02 00:43:44 +0200

    Moved from https://sourceforge.net/p/minilib/

---
+++ lib/MiniCtrls.pas
@@ -0,0 +1,26 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit MiniCtrls;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  PHPUtils, GUIMsgBox, mnFonts, ColorUtils, mnSynUtils, PHPProcessor, 
+  HTMLProcessor, mnSynHighlighterXHTML, mnSynHighlighterStdSQL, 
+  mnSynHighlighterApache, mnSynHighlighterConfig, mnSynHighlighterCpp, 
+  mnSynHighlighterD, mnSynHighlighterFirebird, mnSynHighlighterLua, 
+  mnSynHighlighterMultiProc, mnSynHighlighterGo, mnSynHighlighterSard, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('MiniCtrls', @Register);
+end.
---
+++ native/ntvCtrls.pas
@@ -0,0 +1,19 @@
+unit ntvCtrls; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Messages, Controls, ExtCtrls, SysUtils, Math, Contnrs, Graphics, Forms,
+  LCLType, LCLIntf, LMessages, LCLProc,
+  ntvUtils;
+
+type
+  TntvCustomControl= class(TCustomControl)
+  end;
+
+implementation
+
+end.
+
